0b1a623192a12652b2c318dfd55db856e70127b5
Pedro <105981752+Laogaeth@users.noreply.github.com>
2022-12-13 13:47:18 +0000

    Update MainPage.php

--- 1.MainPage/MainPage.php
+++ 1.MainPage/MainPage.php
@@ -32,7 +32,7 @@
   <!--Nav Menu-->
   <div class="sticky" id="menu">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="./MainPage.html">HOME</a>
+      <a class="navbar-brand" href="./MainPage.php">HOME</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
