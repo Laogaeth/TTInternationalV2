a23a15a90999879d7f2e9a07c80272351b486526
Pedro <pedro3dpereira@gmail.com>
2023-01-21 14:15:24 +0000

    Added arrow to take user back to op of the page

--- Products/javascriptProducts.js
+++ Products/javascriptProducts.js
@@ -19,3 +19,21 @@ $.getJSON("productsData.json", function(data) {
     });
 });
 
+
+//get back to the top button
+
+// Get the button
+const button = document.querySelector('.button--icon');
+const h5Title = document.getElementById('hygiene');
+window.addEventListener('scroll', () => {
+  // Get the position of the h5 title on the page
+  const h5TitlePosition = h5Title.getBoundingClientRect();
+  // Check if the h5 title is out of view
+  if (h5TitlePosition.top < 0) {
+    // Show the button
+    button.style.display = 'block';
+  } else {
+    // Hide the button
+    button.style.display = 'none';
+  }
+});
--- Products/productsPage.php
+++ Products/productsPage.php
@@ -13,7 +13,6 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
   <script src="https://kit.fontawesome.com/9d05ceeaf4.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="../Products/products.css">
-  <script src="../Products/javascriptProducts.js"></script>
   
 </head>
 
@@ -33,6 +32,8 @@
           <a class="nav-item nav-link" href="../6.PortfolioPage/PortfolioPage.php">Products</a> 
           <a class="nav-item nav-link" href="../2.OrçamentoPage/OrçamentoPage.html" >Custom Pricing</a> 
           <a class="nav-item nav-link" href="../3.ContactsPage/Contacts.html">Contacts</a>
+          <a class="nav-item nav-link" href="../3.ContactsPage/Contacts.html">Shopping cart <img  class="icon--shoppingCart" src="./icons/shoppingCart.png" alt=""></a>
+
         </div>
       </div>
     </nav>
@@ -41,35 +42,35 @@
 
   <section>
 
-    <h3 class="section--h3">Our Selection</h3>
+    <h3 id="takeMeBack--button"class="section--h3">Our Selection</h3>
 
     <div class="filters">
-  <a href="#hygiene-products"><button class=" shadow--xs">Hygiene       </button></a>
-  <a href="#food-treats">     <button class=" shadow--xs">Food & Treats </button></a>
-  <a href="#toys">            <button class=" shadow--xs">Toys          </button></a>
-  <a href="#clothes">         <button class=" shadow--xs">Clothes       </button></a>
+  <a href="#hygiene"><button class=" shadow--xs">Hygiene       </button></a>
+  <a href="#foods-div">     <button class=" shadow--xs">Food & Treats </button></a>
+  <a href="#toys-div">            <button class=" shadow--xs">Toys          </button></a>
+  <a href="#clothes-div">         <button class=" shadow--xs">Clothes       </button></a>
     
     
     </div>
 
 
    <div class="container products--title underline">
-    <h5 id="Hygiene">Hygiene</h5>
+    <h5 id="hygiene">Hygiene</h5>
         <div id="hygiene-products" class="row"></div>
 
-    <h5>Foods & Treats</h5>
+    <h5 id="foods-div">Foods & Treats</h5>
         <div id="food-treats" class="row"></div>
 
-    <h5>Toys</h5>
+    <h5 id="toys-div">Toys</h5>
         <div id="toys" class="row"></div>
 
-    <h5>Clothes</h5>
+    <h5 id="clothes-div">Clothes</h5>
         <div id="clothes" class="row"></div>
 
   </div>
 
   <div class="container">
-          <button class="button--icon"><img class="button--icon--arrow" src="./icons/arrowUp.png" alt=""></button>
+          <a href="#takeMeBack--button"><button class="button--icon"><img class="button--icon--arrow" src="./icons/arrowUp.png" alt=""></button></a>
   </div>
         
   </section>
@@ -83,7 +84,10 @@
   <div class="rights">2022 Pedro, Pereira. All rights reserved.</div>
  </div>
 </main>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+ <script src="../Products/javascriptProducts.js"></script>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </body>
 </html>
