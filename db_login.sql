ba753fb5f5f57acd08defc5f2da6a5af02f89b88
Pedro <pedro3dpereira@gmail.com>
2023-01-27 15:14:19 +0000

    PHP external script now will encode data from my db and convert it to json, which will be used by my js script to display it into the browser. After some stress testing now it should be possible to add / erase/edit any data in the db and it'll auto update.
    The amount of issues and headaches php gave me,
    I propose to rename it to volvo.

--- db_login.sql
+++ db_login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Generation Time: Jan 25, 2023 at 06:03 PM
+-- Generation Time: Jan 27, 2023 at 04:13 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -31,18 +31,19 @@ CREATE TABLE `clothes` (
   `id` int(11) NOT NULL,
   `product_name` varchar(255) NOT NULL,
   `product_id` int(11) DEFAULT NULL,
-  `price` decimal(10,2) NOT NULL DEFAULT 5.99
+  `price` decimal(10,2) NOT NULL DEFAULT 5.99,
+  `image_path` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `clothes`
 --
 
-INSERT INTO `clothes` (`id`, `product_name`, `product_id`, `price`) VALUES
-(1, 'Winter Jumper', 4, '5.99'),
-(2, 'Camouflage Jumper', 4, '5.99'),
-(3, 'Melon Jumper', 4, '5.99'),
-(4, 'Festive Cloaks', 4, '5.99');
+INSERT INTO `clothes` (`id`, `product_name`, `product_id`, `price`, `image_path`) VALUES
+(1, 'Winter Jumper', 4, '5.99', './images/clothes1.png'),
+(2, 'Camouflage Jumper', 4, '5.99', './images/clothes2.png'),
+(3, 'Melon Jumper', 4, '5.99', './images/clothes3.png'),
+(4, 'Festive Cloaks', 4, '5.99', './images/clothes4.png');
 
 -- --------------------------------------------------------
 
@@ -54,18 +55,19 @@ CREATE TABLE `food` (
   `id` int(11) NOT NULL,
   `product_name` varchar(255) NOT NULL,
   `product_id` int(11) DEFAULT NULL,
-  `price` decimal(10,2) NOT NULL DEFAULT 5.99
+  `price` decimal(10,2) NOT NULL DEFAULT 5.99,
+  `image_path` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `food`
 --
 
-INSERT INTO `food` (`id`, `product_name`, `product_id`, `price`) VALUES
-(1, 'Purina ONE with chicken', 2, '5.99'),
-(2, 'Smilla Diet with beef', 2, '5.99'),
-(3, 'Concept for life Urinary with chicken', 2, '5.99'),
-(4, 'Concept for Life Hypo Allergenic', 2, '5.99');
+INSERT INTO `food` (`id`, `product_name`, `product_id`, `price`, `image_path`) VALUES
+(1, 'Purina ONE with chicken', 2, '5.99', './images/food1.png'),
+(2, 'Smilla Diet with beef', 2, '5.99', './images/food2.png'),
+(3, 'Concept for life Urinary with chicken', 2, '5.99', './images/food3.png'),
+(4, 'Concept for Life Hypo Allergenic', 2, '5.99', './images/food4.png');
 
 -- --------------------------------------------------------
 
@@ -77,18 +79,19 @@ CREATE TABLE `hygiene` (
   `id` int(11) NOT NULL,
   `product_name` varchar(255) NOT NULL,
   `product_id` int(11) DEFAULT NULL,
-  `price` decimal(10,2) NOT NULL DEFAULT 5.99
+  `price` decimal(10,2) NOT NULL DEFAULT 5.99,
+  `image_path` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `hygiene`
 --
 
-INSERT INTO `hygiene` (`id`, `product_name`, `product_id`, `price`) VALUES
-(1, 'Pet Head Shampoo Orange Scent', 1, '5.99'),
-(2, 'Pet Head Shampoo Peach Scent', 1, '569.00'),
-(3, 'Pet Head Shampoo Pear Scent', 1, '5.99'),
-(4, 'Pet Head Dry Shampoo Coconut Scent', 1, '5.99');
+INSERT INTO `hygiene` (`id`, `product_name`, `product_id`, `price`, `image_path`) VALUES
+(1, 'Pet Head Shampoo Orange Scent', 1, '5.99', './images/hygiene1.png'),
+(2, 'Pet Head Shampoo Peach Scent', 1, '569.00', './images/hygiene2.png'),
+(3, 'Pet Head Shampoo Pear Scent', 1, '5.99', './images/hygiene3.png'),
+(4, 'Pet Head Dry Shampoo Coconut Scent', 1, '5.99', './images/hygiene4.png');
 
 -- --------------------------------------------------------
 
@@ -159,16 +162,32 @@ INSERT INTO `products` (`id`, `category`) VALUES
 
 CREATE TABLE `stock` (
   `id` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `product_id` int(11) DEFAULT NULL
+  `item_id` int(11) NOT NULL,
+  `item_table` varchar(255) NOT NULL,
+  `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `stock`
 --
 
-INSERT INTO `stock` (`id`, `quantity`, `product_id`) VALUES
-(0, 99, 1);
+INSERT INTO `stock` (`id`, `item_id`, `item_table`, `quantity`) VALUES
+(1, 1, 'food', 99),
+(2, 2, 'food', 99),
+(3, 3, 'food', 99),
+(4, 4, 'food', 99),
+(5, 1, 'toys', 99),
+(6, 2, 'toys', 99),
+(7, 3, 'toys', 99),
+(8, 4, 'toys', 99),
+(9, 1, 'hygiene', 99),
+(10, 2, 'hygiene', 99),
+(11, 3, 'hygiene', 99),
+(12, 4, 'hygiene', 99),
+(13, 1, 'clothes', 99),
+(14, 2, 'clothes', 99),
+(15, 3, 'clothes', 99),
+(16, 4, 'clothes', 99);
 
 -- --------------------------------------------------------
 
@@ -180,18 +199,19 @@ CREATE TABLE `toys` (
   `id` int(11) NOT NULL,
   `product_name` varchar(255) NOT NULL,
   `product_id` int(11) DEFAULT NULL,
-  `price` decimal(10,2) NOT NULL DEFAULT 5.99
+  `price` decimal(10,2) NOT NULL DEFAULT 5.99,
+  `image_path` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `toys`
 --
 
-INSERT INTO `toys` (`id`, `product_name`, `product_id`, `price`) VALUES
-(1, 'Squeeky wild mouse', 3, '5.99'),
-(2, 'Squeeky cheese', 3, '5.99'),
-(3, 'Ball with bell', 3, '5.99'),
-(4, 'Chase ball with a bell', 3, '5.99');
+INSERT INTO `toys` (`id`, `product_name`, `product_id`, `price`, `image_path`) VALUES
+(1, 'Squeeky wild mouse', 3, '5.99', './images/toys1.png'),
+(2, 'Squeeky cheese', 3, '5.99', './images/toys2.png'),
+(3, 'Ball with bell', 3, '5.99', './images/toys3.png'),
+(4, 'Chase ball with a bell', 3, '5.99', './images/toys4.png');
 
 -- --------------------------------------------------------
 
@@ -275,7 +295,7 @@ ALTER TABLE `products`
 --
 ALTER TABLE `stock`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `product_id` (`product_id`);
+  ADD KEY `item_id` (`item_id`);
 
 --
 -- Indexes for table `toys`
@@ -301,6 +321,12 @@ ALTER TABLE `user`
 --
 ALTER TABLE `personal_info`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `stock`
+--
+ALTER TABLE `stock`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -347,7 +373,10 @@ ALTER TABLE `personal_info`
 -- Constraints for table `stock`
 --
 ALTER TABLE `stock`
-  ADD CONSTRAINT `stock_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+  ADD CONSTRAINT `stock_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `food` (`id`),
+  ADD CONSTRAINT `stock_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `toys` (`id`),
+  ADD CONSTRAINT `stock_ibfk_3` FOREIGN KEY (`item_id`) REFERENCES `hygiene` (`id`),
+  ADD CONSTRAINT `stock_ibfk_4` FOREIGN KEY (`item_id`) REFERENCES `clothes` (`id`);
 
 --
 -- Constraints for table `toys`
