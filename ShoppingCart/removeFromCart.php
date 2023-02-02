bccaa20366dcec10ca60cf593c669bc63cfdbca0
Pedro <pedro3dpereira@gmail.com>
2023-02-02 19:16:18 +0000

    this is not working,I messed up trying to pass user id. Ill fix it tomorrow

--- ShoppingCart/removeFromCart.php
+++ ShoppingCart/removeFromCart.php
@@ -1,25 +1,38 @@
 <?php
+session_start();
   //connect to the database
   error_reporting(E_ALL);
   ini_set('display_errors', 1);
+
+
   $conn = mysqli_connect("localhost:3307", "root", "", "db_login");
+  $user_id = '';
 
-  //get the cart id from the post data
-  if(isset($_POST['cart_id'])){
-    $cart_id = $_POST['cart_id'];
-  }else{
-    die("Error: Cart ID is not set in POST data.");
-  }
 
-  //remove the item from the cart in the database
-  $query = "DELETE FROM cart WHERE id = '$cart_id'";
-  $result = mysqli_query($conn, $query);
-  
-  //check if the query was successful
-  if (!$result) {
-    die("Query failed: " . mysqli_error($conn));
-  }
+//get the user's ID from the session or query string
+if (isset($_GET['user_id'])) {
+$user_id = intval($_GET['user_id']);
+} else if (isset($_SESSION['user_id'])) {
+$user_id = intval($_SESSION['user_id']);
+}
 
-  //redisplay the updated cart
-  include 'displayCart.php';
+if (isset($user_id)) {
+$query = "DELETE FROM cart WHERE user_id = ?";
+
+$stmt = mysqli_prepare($conn, $query);
+mysqli_stmt_bind_param($stmt, "i", $user_id);
+mysqli_stmt_execute($stmt);
+
+$result = mysqli_stmt_get_result($stmt);
+
+//check if the query was successful
+if (!$result) {
+die("Query failed: " . mysqli_error($conn));
+}
+} else {
+echo "User ID:";
+echo "Query failed:";
+
+}
+
 ?>
