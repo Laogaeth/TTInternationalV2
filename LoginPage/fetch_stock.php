b6eddec7b8ee5ba78a3605b81925a4293b0789bf
Pedro <pedro3dpereira@gmail.com>
2023-02-08 21:37:29 +0000

    lightbulb moment, fixed the previous bug in the stock display

--- LoginPage/fetch_stock.php
+++ LoginPage/fetch_stock.php
@@ -17,45 +17,65 @@ $quantity_data = mysqli_query($conn, "SELECT item_id, quantity FROM stock");
 
 // Combine all data into a single array
 $all_data = array();
+$hygiene_data_array = array();
+$toys_data_array = array();
+$clothes_data_array = array();
+$food_data_array = array();
 
 while ($row = mysqli_fetch_array($hygiene_data)) {
-    $all_data[$row['product_id']] = array(
+    $hygiene_data_array[] = array(
+        'product_id' => $row['product_id'],
         'product_name' => $row['product_name'],
         'price' => $row['price'],
     );
 }
 
 while ($row = mysqli_fetch_array($toys_data)) {
-    $all_data[$row['product_id']] = array(
+    $toys_data_array[] = array(
+        'product_id' => $row['product_id'],
         'product_name' => $row['product_name'],
         'price' => $row['price'],
     );
 }
 
 while ($row = mysqli_fetch_array($clothes_data)) {
-    $all_data[$row['product_id']] = array(
+    $clothes_data_array[] = array(
+        'product_id' => $row['product_id'],
         'product_name' => $row['product_name'],
         'price' => $row['price'],
     );
 }
 
 while ($row = mysqli_fetch_array($food_data)) {
-    $all_data[$row['product_id']] = array(
+    $food_data_array[] = array(
+        'product_id' => $row['product_id'],
         'product_name' => $row['product_name'],
         'price' => $row['price'],
     );
 }
 
+$all_data = array_merge($hygiene_data_array, $toys_data_array, $clothes_data_array, $food_data_array);
+
+$quantity_data_array = array();
 while ($row = mysqli_fetch_array($quantity_data)) {
-    if (isset($all_data[$row['item_id']])) {
-        $all_data[$row['item_id']]['quantity'] = $row['quantity'];
-    }
+$quantity_data_array[$row['item_id']] = $row['quantity'];
 }
-echo json_encode(array_values($all_data));
 
+// Add quantity information to all_data array
+foreach ($all_data as &$data) {
+$product_id = $data['product_id'];
+if (isset($quantity_data_array[$product_id])) {
+$data['quantity'] = $quantity_data_array[$product_id];
+} else {
+$data['quantity'] = 0;
+}
+}
 
-
-// Close the database connection
+// Close the connection
 mysqli_close($conn);
+
+// Return the combined data array
+
+echo json_encode(array_values($all_data));
 
 ?>
