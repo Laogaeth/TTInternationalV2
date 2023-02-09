d9d6f01b9a9456425b9b8698168e0c48d8289029
Pedro <pedro3dpereira@gmail.com>
2023-02-09 14:52:20 +0000

    fetch_stock was flawed, now its a bit more specific on the info that is getting, only issue left to tackle is the quantities being undefined.

--- LoginPage/fetch_stock.php
+++ LoginPage/fetch_stock.php
@@ -10,10 +10,10 @@ if (!$conn) {
 
 // Fetch data from different tables
 $hygiene_data = mysqli_query($conn, "SELECT product_name, price, product_id FROM hygiene");
-$toys_data = mysqli_query($conn, "SELECT product_name, price, product_id FROM toys");
+$toys_data = mysqli_query($conn, "SELECT product_name, price,    product_id FROM toys");
 $clothes_data = mysqli_query($conn, "SELECT product_name, price, product_id FROM clothes");
-$food_data = mysqli_query($conn, "SELECT product_name, price, product_id FROM food");
-$quantity_data = mysqli_query($conn, "SELECT item_id, quantity FROM stock");
+$food_data = mysqli_query($conn, "SELECT product_name, price,    product_id FROM food");
+$stock_data = mysqli_query($conn, "SELECT item_table, hygiene_item_id, food_item_id, toys_item_id, clothes_item_id, quantity FROM stock");
 
 // Combine all data into a single array
 $all_data = array();
@@ -24,58 +24,77 @@ $food_data_array = array();
 
 while ($row = mysqli_fetch_array($hygiene_data)) {
     $hygiene_data_array[] = array(
-        'product_id' => $row['product_id'],
+        'item_id' => $row['product_id'],
         'product_name' => $row['product_name'],
         'price' => $row['price'],
+        'item_table' => 'hygiene',
     );
 }
 
 while ($row = mysqli_fetch_array($toys_data)) {
     $toys_data_array[] = array(
-        'product_id' => $row['product_id'],
+        'item_id' => $row['product_id'],
         'product_name' => $row['product_name'],
         'price' => $row['price'],
+        'item_table' => 'toys',
     );
 }
 
 while ($row = mysqli_fetch_array($clothes_data)) {
     $clothes_data_array[] = array(
-        'product_id' => $row['product_id'],
+        'item_id' => $row['product_id'],
         'product_name' => $row['product_name'],
         'price' => $row['price'],
+        'item_table' => 'clothes',
     );
 }
 
 while ($row = mysqli_fetch_array($food_data)) {
     $food_data_array[] = array(
-        'product_id' => $row['product_id'],
+        'item_id' => $row['product_id'],
         'product_name' => $row['product_name'],
         'price' => $row['price'],
+        'item_table' => 'food',
     );
 }
 
 $all_data = array_merge($hygiene_data_array, $toys_data_array, $clothes_data_array, $food_data_array);
 
-$quantity_data_array = array();
-while ($row = mysqli_fetch_array($quantity_data)) {
-$quantity_data_array[$row['item_id']] = $row['quantity'];
+$stock_data_array = array();
+while ($row = mysqli_fetch_array($stock_data)) {
+    $item_id = $row['item_table'];
+    $quantity = $row['quantity'];
+    $stock_data_array[$item_id] = $quantity;
 }
-
-// Add quantity information to all_data array
 foreach ($all_data as &$data) {
-$product_id = $data['product_id'];
-if (isset($quantity_data_array[$product_id])) {
-$data['quantity'] = $quantity_data_array[$product_id];
-} else {
-$data['quantity'] = 0;
+$item_id = $data['item_table'];
+$table_name = '';
+switch($item_id) {
+case in_array($item_id, array_column($hygiene_data_array, 'item_id')):
+$table_name = 'hygiene';
+break;
+case in_array($item_id, array_column($toys_data_array, 'item_id')):
+$table_name = 'toys';
+break;
+case in_array($item_id, array_column($clothes_data_array, 'item_id')):
+$table_name = 'clothes';
+break;
+case in_array($item_id, array_column($food_data_array, 'item_id')):
+$table_name = 'food';
+break;
+default:
+$table_name = '';
+break;
+}
+if (!empty($table_name)) {
+$quantity_value = $quantity_data_array[$table_name][$item_id] ??= 0;
+$data['quantity'] = $quantity_value;
 }
 }
 
-// Close the connection
+// Close database connection
 mysqli_close($conn);
-
-// Return the combined data array
-
 echo json_encode(array_values($all_data));
-
+// Return all_data array
+return $all_data;
 ?>
