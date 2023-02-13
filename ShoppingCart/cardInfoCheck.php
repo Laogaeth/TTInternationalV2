aeb07b6ba837a241545c653be0e4af652dda7f21
Pedro <pedro3dpereira@gmail.com>
2023-02-13 09:32:12 +0000

    Fixed a small big where it wasnt redirecting the user to the the credit card form when they didnt have a card on file.

--- ShoppingCart/cardInfoCheck.php
+++ ShoppingCart/cardInfoCheck.php
@@ -1,19 +1,19 @@
-<?php 
+<?php
 
 
-$hasCreditCard = false;
+// Connect to the database
+$db = mysqli_connect('localhost:3307', 'root', '', 'db_login');
 
-// Check if user has a credit card stored in the database
+$hasCreditCard = true;
+
 if (isset($_SESSION['user_id'])) {
-  // Connect to the database
-  $db = mysqli_connect('localhost:3307', 'root', '', 'db_login');
+  $user_id = $_SESSION["user_id"];
+  $query = "SELECT user_id FROM credit_card WHERE user_id = $user_id AND card_number IS NOT NULL";
+  $result = mysqli_query($db, $query);
 
-
-$user_id = $_SESSION["user_id"];
-$query = "SELECT user_id FROM credit_card WHERE user_id = $user_id AND card_number IS NOT NULL";
-$result = mysqli_query($db, $query);
+  if (mysqli_num_rows($result) > 0) {
+    $hasCreditCard = false;
+  }
 
   mysqli_close($db);
 }
-
-?>
